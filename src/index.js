b7c234cb05c7e280fc3b695978de7f9e5f338177
Samira77-ss <samiraseyfii0709@gmail.com>
2023-03-02 13:27:28 +0330

    Added temperature function

--- src/index.js
+++ src/index.js
@@ -18,14 +18,29 @@ function formatDay(timestamp) {
   let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
   return days[day];
 }
-
-function displayTemperature(response) {
-  console.log(response.date);
+function displayWeatherCondition(response) {
+  console.log(response);
+  let temperatureElement = document.querySelector("#temperature");
   let dateElement = document.querySelector("#date");
-  dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let iconElement = document.querySelector("#icon");
+  celsiusTemperature = response.data.temperature.current;
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+  cityElement.innerHTML = response.data.city;
+  descriptionElement = response.data.condition.description;
+  humidityElement.innerHTML = response.data.temperature.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  dateElement.innerHTML = formatDate(response.data.time * 1000);
+  iconElement.setAttribute(
+    "src",
+    `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`
+  );
+  iconElement.setAttribute("alt", response.data.condition.description);
 }
-function search(city) {
-  let apiKey = "bo2f99f4b4829bb67942fdad243367t0";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&unites=metric`;
-  axios.get(apiUrl).then(displayTemperature);
-}
+
+let apiKey = "bo2f99f4b4829bb67942fdad243367t0";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query={query}&key=${apiKey}`;
+axios.get(apiUrl).then(displayWeatherCondition);
